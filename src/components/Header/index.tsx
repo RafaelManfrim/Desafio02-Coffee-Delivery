fd2d13724f747c5c160d15afc9613ee241bb2c39
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-07-15 10:59:57 -0300

    Visual Header finalizado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,27 @@
+import { MapPin, ShoppingCart } from 'phosphor-react'
+import logoCoffeeDelivery from '../../assets/logo.svg'
 import styles from './styles.module.scss'
 
 export function Header() {
   return (
-    <div className={styles['header-container']}>
-      Header
+    <div className={styles.headerContainer}>
       <div>
-        <div>local</div>
-        <div>cart</div>
+        <img
+          src={logoCoffeeDelivery}
+          alt="Logo da Coffee Delivery contendo um copo roxo com um desenho de um foguete no meio."
+        />
+      </div>
+      <div className={styles.headerMenu}>
+        <button className={styles.location}>
+          <MapPin size={22} weight="fill" />
+          Novo Horizonte, SP
+        </button>
+        <div className={styles.cart}>
+          <button className={styles.cartButton}>
+            <ShoppingCart size={22} weight="fill" />
+          </button>
+          <span className={styles.cartBadge}>3</span>
+        </div>
       </div>
     </div>
   )
