d5672a52bbab53b319731088aa3308a3df9e616b
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-07-18 00:36:27 -0300

    Estrutura html página Checkout e página Success

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,5 +1,62 @@
+import { CurrencyDollar, MapPinLine } from 'phosphor-react'
+
 import styles from './styles.module.scss'
 
 export function Checkout() {
-  return <div>Checkout</div>
+  return (
+    <div>
+      <div>
+        <h4>Complete seu pedido</h4>
+        <div>
+          <div>
+            <MapPinLine />
+            <div>
+              <span>Endereço de Entrega</span>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </div>
+          </div>
+          <div>
+            <input type="text" placeholder="CEP" />
+            <input type="text" placeholder="Rua" />
+            <input type="text" placeholder="Número" />
+            <input type="text" placeholder="Complemento" />
+            <input type="text" placeholder="Bairro" />
+            <input type="text" placeholder="Cidade" />
+            <input type="text" placeholder="UF" />
+          </div>
+        </div>
+        <div>
+          <div>
+            <CurrencyDollar />
+            <div>
+              <span>Pagamento</span>
+              <p>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </p>
+            </div>
+          </div>
+          <div>
+            <input type="text" placeholder="Cartão de crédito" />
+            <input type="text" placeholder="cartão de débito" />
+            <input type="text" placeholder="dinheiro" />
+          </div>
+        </div>
+      </div>
+      <div>
+        <h4>Cafés selecionados</h4>
+        <div>
+          <div>Carrinho</div>
+          <div>
+            <span>Total de itens</span>
+            <span>R$ 29,70</span>
+            <span>Entrega</span>
+            <span>R$ 3,50</span>
+            <strong>Total</strong>
+            <strong>R$ 33,20</strong>
+          </div>
+          <button>confirmar pedido</button>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,5 +1,53 @@
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
+
+import motoboyImg from '../../assets/motoboy.svg'
+
 import styles from './styles.module.scss'
 
 export function Success() {
-  return <div>Success</div>
+  return (
+    <div>
+      <div>
+        <h2>Uhu! Pedido confirmado</h2>
+        <p>Agora é só aguardar que logo o café chegará até você</p>
+        <div>
+          <div>
+            <div>
+              <MapPin weight="fill" />
+            </div>
+            <div>
+              <span>
+                Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+              </span>
+              <span>Farrapos - Porto Alegre, RS</span>
+            </div>
+          </div>
+          <div>
+            <div>
+              <Timer weight="fill" />
+            </div>
+            <div>
+              <span>Previsão de entrega</span>
+              <strong>20 min - 30 min</strong>
+            </div>
+          </div>
+          <div>
+            <div>
+              <CurrencyDollar />
+            </div>
+            <div>
+              <span>Pagamento na entrega</span>
+              <strong>Cartão de Crédito</strong>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <img
+          src={motoboyImg}
+          alt="Entregador em uma moto azul levando um pedido da Coffee Delivery"
+        />
+      </div>
+    </div>
+  )
 }
