a79bbee709fa80813af348ea2bd1f361df5b950b
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-07-18 12:08:01 -0300

    Visual tela Success

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,53 +1,56 @@
 import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
 
 import motoboyImg from '../../assets/motoboy.svg'
+import { useWindowDimensions } from '../../hooks/useWindowDimensions'
 
 import styles from './styles.module.scss'
 
 export function Success() {
+  const { width } = useWindowDimensions()
+
   return (
-    <div>
-      <div>
+    <div className={styles.successContainer}>
+      <div className={styles.successContainerInformations}>
         <h2>Uhu! Pedido confirmado</h2>
         <p>Agora é só aguardar que logo o café chegará até você</p>
-        <div>
-          <div>
-            <div>
-              <MapPin weight="fill" />
+        <div className={styles.orderInformation}>
+          <div className={styles.information}>
+            <div className={`${styles.iconCircle} ${styles.bgPurple}`}>
+              <MapPin weight="fill" size={16} />
             </div>
-            <div>
+            <div className={styles.informationText}>
               <span>
                 Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
               </span>
               <span>Farrapos - Porto Alegre, RS</span>
             </div>
           </div>
-          <div>
-            <div>
-              <Timer weight="fill" />
+          <div className={styles.information}>
+            <div className={`${styles.iconCircle} ${styles.bgYellow}`}>
+              <Timer weight="fill" size={16} />
             </div>
-            <div>
+            <div className={styles.informationText}>
               <span>Previsão de entrega</span>
               <strong>20 min - 30 min</strong>
             </div>
           </div>
-          <div>
-            <div>
-              <CurrencyDollar />
+          <div className={styles.information}>
+            <div className={`${styles.iconCircle} ${styles.bgYellowDark}`}>
+              <CurrencyDollar size={16} />
             </div>
-            <div>
+            <div className={styles.informationText}>
               <span>Pagamento na entrega</span>
               <strong>Cartão de Crédito</strong>
             </div>
           </div>
         </div>
       </div>
-      <div>
+      {width > 1280 && (
         <img
           src={motoboyImg}
           alt="Entregador em uma moto azul levando um pedido da Coffee Delivery"
         />
-      </div>
+      )}
     </div>
   )
 }
